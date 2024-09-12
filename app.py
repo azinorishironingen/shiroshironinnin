1456e7b5f89977a04ef68902fd30c6f5c1843dd4
azinorishironingen <aoi.taguchi.m@gmail.com>
2024-09-12 19:07:41 +0900

    aoi

--- app.py
+++ app.py
@@ -4,4 +4,13 @@ app=Flask(__name__)
 @app.route("/")
 def top():
     return render_template("top.html")
+
+@app.route("/add")
+def add():
+    return render_template("add.html")
+
+@app.route("/edit")
+def edit():
+    return render_template("edit.html")
+
 app.run(debug=True)
