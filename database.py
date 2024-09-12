77002621f688a837733d1c2958495edd52503a2f
azinorishironingen <aoi.taguchi.m@gmail.com>
2024-09-12 19:29:50 +0900

    aoi

---
+++ database.py
@@ -0,0 +1,13 @@
+from peewee import *
+
+db = SqliteDatabase('ajinoris.db')
+
+class Ajinori(Model):
+    name = CharField()
+
+    class Meta:
+        database = db # This model uses the "people.db" database.
+
+db.create_tables([Ajinori])
+
+Ajinori.create(name="白白人間")
